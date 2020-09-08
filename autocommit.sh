11a69a20eff856f47b7492d3a2dc88815c6a0fad
notzodiackiller <cutechickadees@gmail.com>
2020-09-08 17:50:22 -0600

    v_20200908 17:50:22

--- autocommit.sh
+++ autocommit.sh
@@ -3,5 +3,5 @@ cd /Users/elaineyao/Desktop/Projects/website1
 git add .
 git commit -m "v_$(date +'%Y%m%d %T')"
 git push origin master
-bash /Users/elaineyao/Desktop/Projects/website1/autocommit.sh
+
 echo "autocommit completed"
